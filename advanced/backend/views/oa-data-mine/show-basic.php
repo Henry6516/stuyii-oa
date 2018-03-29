75251be1e8feade97f5793bc24d868cf1bc26c0f
turpure <p812816627@163.com>
2018-03-29 10:24:54 +0800

    back to top

--- advanced/backend/views/oa-data-mine/show-basic.php
+++ advanced/backend/views/oa-data-mine/show-basic.php
@@ -163,12 +163,19 @@ echo "
                     <template scope="scope">
                         <image :src="scope.row.varMainImage" width="50" height="50"/>
                     </template>
-                </el-table-column>e
+                </el-table-column>
             </el-table>
+
         </template>
+        <div style="margin-top: 3%; margin-left: 5%">
+            <el-row>
+                <el-button type="primary" round>保存数据</el-button>
+                <el-button type="success" round>导出数据</el-button>
+            </el-row>
+        </div>
     </div>
 </div>
-
+<a href="#" id="back-to-top" title="Back to top">&uarr;</a>
 <?php ActiveForm::end() ?>
 
 
@@ -204,7 +211,35 @@ echo "
     .el-table__body input {
         border-radius: 15px ;
     }
+
+
+    #back-to-top {
+        position: fixed;
+        bottom: 40px;
+        right: 40px;
+        z-index: 9999;
+        width: 40px;
+        height: 40px;
+        text-align: center;
+        line-height: 30px;
+        background: #f5f5f5;
+        color: #444;
+        cursor: pointer;
+        border: 0;
+        border-radius: 5px;
+        text-decoration: none;
+        transition: opacity 0.2s ease-out;
+        opacity: 0;
+    }
+    #back-to-top:hover {
+        background: #e9ebec;
+    }
+    #back-to-top.show {
+        opacity: 1;
+    }
 </style>
+
+
 
 <script>
 
@@ -228,3 +263,34 @@ echo "
 
 </script>
 
+
+<?php
+$js = <<<JS
+
+if ($('#back-to-top').length) {
+    var scrollTrigger = 100, // px
+        backToTop = function () {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop > scrollTrigger) {
+                $('#back-to-top').addClass('show');
+            } else {
+                $('#back-to-top').removeClass('show');
+            }
+        };
+    backToTop();
+    $(window).on('scroll', function () {
+        backToTop();
+    });
+    $('#back-to-top').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
+}
+
+JS;
+
+
+$this->registerJs($js);
+?>
