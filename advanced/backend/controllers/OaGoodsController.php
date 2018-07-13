1b807f29ddc40b811a913a97f7c250e45e857ef9
turpure <p812816627@163.com>
2018-07-13 15:48:07 +0800

    can stock up or not ?

--- advanced/backend/controllers/OaGoodsController.php
+++ advanced/backend/controllers/OaGoodsController.php
@@ -6,6 +6,7 @@ use backend\models\GoodsCats;
 use backend\models\OaForwardGoods;
 use backend\models\OaBackwardGoods;
 use backend\models\OaGoodsinfo;
+use backend\models\User;
 use common\components\BaseController;
 use PHPUnit\Framework\Exception;
 use Yii;
@@ -966,6 +967,14 @@ class OaGoodsController extends BaseController
     private function validateStock()
     {
         $user = Yii::$app->user->identity->username;
+        $userid = Yii::$app->user->identity->getId();
+        $User = User::findOne(['id'=>$userid]);
+        $canStock = $User->canStockUp?:0;
+
+        //备货的人才接受检查
+        if ($canStock === 0){
+            return 'no';
+        }
         $stockUsed = 'select count(og.nid) as usedStock  from oa_goods as og  
                       LEFT JOIN oa_goodsinfo as ogs on og.nid = ogs.goodsid
                       where og.stockUp=1 and og.developer=:developer 
@@ -976,11 +985,20 @@ class OaGoodsController extends BaseController
                       and DATEDIFF(mm, createDate, getdate()) = 0
                       and developer=:developer";
         $connection = Yii::$app->db;
-        $used = $connection->createCommand($stockUsed,[':developer'=>$user])->queryAll()[0]['usedStock'];
-        $have = $connection->createCommand($stockHave,[':developer'=>$user])->queryAll();
-        if(!$have){
-            return 'no';
-        }else if($used>=$have[0]['haveStock']) {
+
+        try {
+            $used = $connection->createCommand($stockUsed,[':developer'=>$user])->queryAll()[0]['usedStock'];
+        }
+        catch (\Exception $e) {
+            $used = 0;
+        }
+        try {
+            $have = $connection->createCommand($stockHave,[':developer'=>$user])->queryAll()[0]['haveStock'];
+        }
+        catch (\Exception $e) {
+            $have = 0;
+        }
+        if($have>0  && $have<=$used ) {
            return 'no';
         }
         return 'yes';
@@ -989,6 +1007,14 @@ class OaGoodsController extends BaseController
     private function validateCreate()
     {
         $user = Yii::$app->user->identity->username;
+        $userid = Yii::$app->user->identity->getId();
+        $User = User::findOne(['id'=>$userid]);
+        $canStock = $User->canStockUp?:0;
+
+        //不备货的人才接受检查
+        if ($canStock > 0){
+            return 'yes';
+        }
         $numberUsed = 'select count(og.nid) as usedStock  from oa_goods as og  
                       LEFT JOIN oa_goodsinfo as ogs on og.nid = ogs.goodsid
                       where isnull(og.stockUp,0)=0 and og.developer=:developer 
@@ -1012,7 +1038,7 @@ class OaGoodsController extends BaseController
             $have = 0;
         }
 
-         if($have>0 && $used>=$have) {
+         if($have>0  && $have<=$used) {
             return 'no';
         }
         return 'yes';
